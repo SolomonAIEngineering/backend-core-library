ab157387575c279cfa241b4ffda6d4874b87ba72
yoan <yoanyombapro@gmail.com>
2023-09-18 21:10:06 -0400

    bumping version from 1.0.7 to 1.0.8

--- version.go
+++ version.go
@@ -1,20 +1,11 @@
-// Copyright The OpenTelemetry Authors
+// Copyright (C) Simfiny, Inc. 2022-present.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-package corelib // import "github.com/SolomonAIEngineering/backend-core-library"
+package corelib
 
 // Version is the current release version of OpenTelemetry in use.
-func Version() string {
-	return "1.0.7"
-}
+var VERSION = "1.0.8"
+var REVISION = "unknown"
