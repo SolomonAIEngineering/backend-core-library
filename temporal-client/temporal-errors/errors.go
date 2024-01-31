a4c9e40a931a0c240f1e0744eda2b95236565a86
yoan <yoanyombapro@gmail.com>
2024-01-31 08:57:39 -0500

    feat: add new client and message queue
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- temporal-client/temporal-errors/errors.go
+++ temporal-client/temporal-errors/errors.go
@@ -4,10 +4,15 @@ import "errors"
 
 var (
 	// ErrInvalidInput is returned when the input provided to a workflow is invalid.
-	ErrInvalidInput                   = errors.New("invalid input")
-	ErrNilWorkerConfig                = errors.New("worker config is nil")
-	ErrNilClient                      = errors.New("client is nil")
-	ErrEmptyTaskQueue                 = errors.New("task queue is empty")
+	ErrInvalidInput = errors.New("invalid input")
+	// ErrInvalidWorkflowID is returned when the workflow ID is invalid.
+	ErrNilWorkerConfig = errors.New("worker config is nil")
+	// ErrInvalidWorkflowID is returned when the workflow ID is invalid.
+	ErrNilClient = errors.New("client is nil")
+	// ErrInvalidWorkflowID is returned when the workflow ID is invalid.
+	ErrEmptyTaskQueue = errors.New("task queue is empty")
+	// ErrInvalidWorkflowID is returned when the workflow ID is invalid.
 	ErrInvalidMaxConcurrentActivities = errors.New("max concurrent activities is invalid")
-	ErrInvalidMaxConcurrentWorkflows  = errors.New("max concurrent workflows is invalid")
+	// ErrInvalidWorkflowID is returned when the workflow ID is invalid.
+	ErrInvalidMaxConcurrentWorkflows = errors.New("max concurrent workflows is invalid")
 )
